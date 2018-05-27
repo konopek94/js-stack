f6df8bdcec93f1caeeb1eee653d4b030562edde7
konopek94 <212954@student.pwr.edu.pl>
2018-05-27 14:55:43 +0200

    react included

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,10 @@
-//@flow 
+// @flow
 
 import compression from 'compression'
 import express from 'express'
 
-import {
-    APP_NAME,
-    STATIC_PATH,
-    WEB_PORT
-} from '../shared/config'
-import {
-    isProd
-} from '../shared/util'
+import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
+import { isProd } from '../shared/util'
 import renderApp from './render-app'
 
 const app = express()
@@ -20,10 +14,11 @@ app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send(renderApp(APP_NAME))
+  res.send(renderApp(APP_NAME))
 })
 
 app.listen(WEB_PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development)'}.`)
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
+  '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
 })
